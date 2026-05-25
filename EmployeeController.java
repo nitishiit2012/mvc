c5d78ec61326c5d67ba910cecd740568a7689a38
Nitish Gupta <nitishiit2012@gmail.com>
2026-05-26 00:13:13 +0530

    files cnages

--- EmployeeController.java
+++ EmployeeController.java
@@ -1,4 +1,36 @@
 package com.codingshuttle.nitish.mvc.controller;
 
+import com.codingshuttle.nitish.mvc.dto.EmployeeDTO;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+
+@RestController
+@RequestMapping("/employee")
 public class EmployeeController {
+
+    @GetMapping(path="/getsecretmessage")
+    public String getSecretMessage(){
+        return "Secret Message: asdffghh";
+    }
+
+    //@GetMapping(path="/getEmployee/Details/{emp_id}")
+    @GetMapping(path="/{emp_id}")
+    public EmployeeDTO getEmployeeDetails( @PathVariable(name= "emp_id") Long p_emp_id){
+     return new EmployeeDTO(p_emp_id,"Nitish","nitish@gmail.com",27, LocalDate.of(2024,1,2),true);
+    }
+
+    //@GetMapping(path="/getEmployee/Details")
+    @GetMapping()
+    public String getAllEmployees(@RequestParam(required = false, name="inp_filter_by") String p_filter_by, @RequestParam(required = false)  String p_order_by ){
+        return "Hi employees are filter by "+ p_filter_by + " and sorted by " + p_order_by;
+    }
+
+    @PostMapping
+    public EmployeeDTO postEmployee( @RequestBody EmployeeDTO emp){
+
+        emp.setId(100L);
+
+        return emp;
+    }
 }
